eb0e236f68f8fd912e6698ddd0e2d3fdeff8582d
saknarin3552 <85098183+saknarin3552@users.noreply.github.com>
2021-08-21 16:20:43 +0700

    Add files via upload

---
+++ CRT_Scene.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CRT_Scene : MonoBehaviour
+{
+    public void LoadScene(string nameScene)
+    {
+        SceneManager.LoadScene(nameScene);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
